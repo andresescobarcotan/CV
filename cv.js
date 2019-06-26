75c96d2c63733858be7659c1027010980d56306f
Andres Escobar Cotan <aescobac@everis.com>
2019-06-26 18:24:31 +0200

    Several changes in CSS/Html

--- cv.js
+++
@@ -1,8 +0,0 @@
-var curriculum = '{"nombre" : "Andres Escobar Cotan","titulo" : "Ingeniero Superior en Informatica"}';
-
-function procesarCurriculum(){  
-    var jsonCurriculum = JSON.parse(curriculum);
-};
-
-
-procesarCurriculum();
